842a5995eb79444f0ed3f5946aea536993348a7a
paplauskis <paplauskaskostas@gmail.com>
2023-07-19 20:30:15 +0300

    wrote test for Ship factory function

---
+++ tests/ship-methods.test.js
@@ -0,0 +1,29 @@
+const Ship = require('../src/index')
+
+test('should correctly tack hits taken', () => {
+  const myShip = Ship(3)
+
+  expect(myShip.hitsTaken).toBe(0)
+
+  myShip.hit();
+  expect(myShip.hitsTaken).toBe(1);
+
+  myShip.hit();
+  expect(myShip.hitsTaken).toBe(2);
+
+  myShip.hit();
+  expect(myShip.hitsTaken).toBe(3);
+}) 
+
+test('should correctly tack hits taken', () => {
+  const myShip = Ship(2)
+
+  expect(myShip.isSunk()).toBe(false)
+
+  myShip.hit();
+  expect(myShip.isSunk()).toBe(false);
+
+  myShip.hit();
+  expect(myShip.isSunk()).toBe(true);
+
+})
