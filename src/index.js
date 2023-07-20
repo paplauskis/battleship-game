9a2db84ef5a1cc50bcf3a68c9c732d40b7cfd930
paplauskis <paplauskaskostas@gmail.com>
2023-07-20 17:38:07 +0300

    created createGameboard, identifyShip functions and Gameboard factory function
    createGameboard function creates gameboard with coordinates going from a1 to j10, identifyShip function creates certain type of Ship based on its name, Gameboard factory function controls both of these functions and assigns coordinates to certain ship

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ function Ship(length) {
     return length === hitsTaken
   }
   return {
+    length,
     hit,
     get hitsTaken() {
       return hitsTaken
@@ -16,4 +17,51 @@ function Ship(length) {
   }
 }
 
-module.exports = Ship
+function createGameboard() {
+  const letterCoords = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+  const numberCoords = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+  let coords = []
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const element = {}
+      element.name = letterCoords[i].concat(numberCoords[j])
+      element.value = null
+      coords.push(element)
+    }
+  }
+  return coords
+}
+
+function identifyShip(shipType) {
+  return shipType.toLowerCase() === 'carrier'
+    ? Ship(5)
+    : shipType.toLowerCase() === 'battleship'
+    ? Ship(4)
+    : shipType.toLowerCase() === 'submarine'
+    ? Ship(3)
+    : shipType.toLowerCase() === 'destroyer'
+    ? Ship(2)
+    : (alert("A ship with this name doesn't exist"), null)
+}
+
+function Gameboard() {
+  const coordinates = createGameboard()
+
+  const placeShip = (shipType, coord) => {
+    const ship = identifyShip(shipType)
+    placeShipOnCoordinate(ship, coord)
+  }
+
+  const placeShipOnCoordinate = (ship, coord) => {
+    coordinates.forEach((item) => {
+      if (item.name === coord) {
+        ship.value = coord
+        item.value = ship
+      }
+    })
+  }
+  return { placeShip, coordinates }
+}
+
+module.exports = { Ship, identifyShip, createGameboard, Gameboard }
