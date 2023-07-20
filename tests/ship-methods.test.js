5f0a938b0e6f86527ed55d824c8a24982c93ff56
paplauskis <paplauskaskostas@gmail.com>
2023-07-20 17:32:28 +0300

    fixed typos

--- tests/ship-methods.test.js
+++ tests/ship-methods.test.js
@@ -1,29 +1,35 @@
-const Ship = require('../src/index')
+const { Ship } = require('../src/index')
 
 test('should correctly tack hits taken', () => {
   const myShip = Ship(3)
 
   expect(myShip.hitsTaken).toBe(0)
 
-  myShip.hit();
-  expect(myShip.hitsTaken).toBe(1);
+  myShip.hit()
+  expect(myShip.hitsTaken).toBe(1)
 
-  myShip.hit();
-  expect(myShip.hitsTaken).toBe(2);
+  myShip.hit()
+  expect(myShip.hitsTaken).toBe(2)
 
-  myShip.hit();
-  expect(myShip.hitsTaken).toBe(3);
-}) 
+  myShip.hit()
+  expect(myShip.hitsTaken).toBe(3)
+})
 
-test('should correctly tack hits taken', () => {
-  const myShip = Ship(2)
+test('should correctly track if ship is sunk', () => {
+  const myShip = Ship(4)
 
   expect(myShip.isSunk()).toBe(false)
 
-  myShip.hit();
-  expect(myShip.isSunk()).toBe(false);
+  myShip.hit()
+  expect(myShip.isSunk()).toBe(false)
 
-  myShip.hit();
-  expect(myShip.isSunk()).toBe(true);
+  myShip.hit()
+  expect(myShip.isSunk()).toBe(false)
 
+  myShip.hit()
+  expect(myShip.isSunk()).toBe(false)
+
+  myShip.hit()
+  expect(myShip.isSunk()).toBe(true)
 })
+
