e0eecc79b833c893e65197ae5f1662255b644352
paplauskis <paplauskaskostas@gmail.com>
2023-07-21 13:12:39 +0300

    wrote more tests and made older ones clearer

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,43 +1,108 @@
-const { Gameboard, createGameboard, identifyShip } = require('../src/index')
+const { Gameboard } = require('../src/index')
 
-test('should place the ship correctly on the gameboard', () => {
+test('should correctly place carrier on coordinates', () => {
   const gameboard = Gameboard()
 
-  gameboard.placeShip('carrier', 'b2')
+  gameboard.placeShip('carrier', 'b5')
 
   const updatedCoordinates = gameboard.coordinates
 
+  expect(updatedCoordinates[10].value).toMatchObject({
+    length: 5,
+  })
+
   expect(updatedCoordinates[11].value).toMatchObject({
     length: 5,
-    hitsTaken: 0,
-    isSunk: expect.any(Function),
+  })
+
+  expect(updatedCoordinates[12].value).toMatchObject({
+    length: 5,
+  })
+
+  expect(updatedCoordinates[13].value).toMatchObject({
+    length: 5,
+  })
+
+  expect(updatedCoordinates[14].value).toMatchObject({
+    length: 5,
   })
 })
 
-test('should place different ships correctly on the gameboard', () => {
+test('should correctly place battleship on coordinates', () => {
   const gameboard = Gameboard()
 
-  gameboard.placeShip('battleship', 'a1')
-  gameboard.placeShip('submarine', 'j10')
-  gameboard.placeShip('destroyer', 'f9')
+  gameboard.placeShip('battleship', 'a4')
 
   const updatedCoordinates = gameboard.coordinates
 
   expect(updatedCoordinates[0].value).toMatchObject({
     length: 4,
-    hitsTaken: 0,
-    isSunk: expect.any(Function),
+  })
+
+  expect(updatedCoordinates[1].value).toMatchObject({
+    length: 4,
+  })
+
+  expect(updatedCoordinates[2].value).toMatchObject({
+    length: 4,
+  })
+
+  expect(updatedCoordinates[3].value).toMatchObject({
+    length: 4,
+  })
+})
+
+test('should correctly place submarine on coordinates', () => {
+  const gameboard = Gameboard()
+
+  gameboard.placeShip('submarine', 'j10')
+
+  const updatedCoordinates = gameboard.coordinates
+
+  expect(updatedCoordinates[97].value).toMatchObject({
+    length: 3,
+  })
+
+  expect(updatedCoordinates[98].value).toMatchObject({
+    length: 3,
   })
 
   expect(updatedCoordinates[99].value).toMatchObject({
     length: 3,
-    hitsTaken: 0,
-    isSunk: expect.any(Function),
   })
+})
+
+test('should correctly place destroyer on coordinates', () => {
+  const gameboard = Gameboard()
+
+  gameboard.placeShip('destroyer', 'f9')
+
+  const updatedCoordinates = gameboard.coordinates
 
   expect(updatedCoordinates[58].value).toMatchObject({
     length: 2,
-    hitsTaken: 0,
-    isSunk: expect.any(Function),
   })
+
+  expect(updatedCoordinates[57].value).toMatchObject({
+    length: 2,
+  })
+})
+
+test('should not place ships if they go out of the board (too high)', () => {
+  const gameboard = Gameboard()
+
+  gameboard.placeShip('carrier', 'c3')
+  gameboard.placeShip('battleship', 'e2')
+  gameboard.placeShip('submarine', 'f1')
+  gameboard.placeShip('destroyer', 'j1')
+
+  const updatedCoordinates = gameboard.coordinates
+
+  expect(updatedCoordinates[22].value).toBeNull()
+
+  expect(updatedCoordinates[41].value).toBeNull()
+
+  expect(updatedCoordinates[50].value).toBeNull()
+
+  expect(updatedCoordinates[90].value).toBeNull()
 })
