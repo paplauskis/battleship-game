38901952b171e18c8a626b0116f417d3f6ae1d6c
paplauskis <paplauskaskostas@gmail.com>
2023-07-25 18:12:05 +0300

    created Player and Computer factory functions
    also added getComputerAttack function which select random and unattacked coordinate. Slightly changed checkIfAllShipsAreSunk to return true/false

--- src/index.js
+++ src/index.js
@@ -50,41 +50,40 @@ function Gameboard() {
 
   const placeShip = (shipType, coord) => {
     const ship = identifyShip(shipType)
-    
+
     if (checkIfShipFits(ship, coord)) {
       placeShipOnCoordinate(ship, coord)
     } else console.log("ship doesn't fit in desired coordinate")
   }
-  
+
   const checkIfShipFits = (ship, coord) => {
     return ship.length <= coord.substring(1)
   }
-  
+
   const placeShipOnCoordinate = (ship, coord, placedCells = 0) => {
     if (!Array.isArray(ship.value)) {
       ship.value = []
     }
-    
+
     coordinates.forEach((item) => {
       if (item.name === coord) {
         ship.value.push(coord)
         item.value = ship
       }
     })
-    
+
     if (ship.length + placedCells <= ship.length) {
       let newCord = coord.substring(1)
       for (let i = 2; i <= ship.length; i++) {
         const newCoord = `${coord.substring(0, 1)}${--newCord}`
         placeShipOnCoordinate(ship, newCoord, ++placedCells)
       }
-    } 
+    }
   }
 
   function searchForCoordinate(chosenCoord) {
     for (let i = 0; i < coordinates.length; i++) {
-      if (chosenCoord === coordinates[i].name)
-        return coordinates[i]
+      if (chosenCoord === coordinates[i].name) return coordinates[i]
     }
   }
 
@@ -92,34 +91,72 @@ function Gameboard() {
     const attackedCoord = searchForCoordinate(chosenCoord)
     if (attackedCoord.value === null) {
       attackedCoord.value = 'missed'
-    } else if (typeof attackedCoord.value === 'object'){
+    } else if (typeof attackedCoord.value === 'object') {
       attackedCoord.value.hit()
       if (attackedCoord.value.isSunk()) console.log('Ship has been sunk')
       attackedCoord.value = 'hit'
-    } else alert('Cannot hit the same coordinate twice!')
-
+    } else console.log('Cannot hit the same coordinate twice!')
   }
 
   const checkIfAllShipsAreSunk = () => {
-    let remainingShipCoords = 0;
-    coordinates.forEach(item => {
-      if(typeof item.value === 'object' && item.value !== null)
+    let remainingShipCoords = 0
+    coordinates.forEach((item) => {
+      if (typeof item.value === 'object' && item.value !== null)
         remainingShipCoords++
     })
-    if (remainingShipCoords === 0)
-      return 'all ships have been sunk'
+    return remainingShipCoords === 0
   }
 
   return { placeShip, coordinates, receiveAttack, checkIfAllShipsAreSunk }
 }
 
-// const myGameboard = Gameboard()
+function Player() {
+  return {
+    attack: (enemyGameboard, chosenCoord) => {
+      enemyGameboard.receiveAttack(chosenCoord)
+    },
+  }
+}
+
+function Computer() {
+  return {
+    attack: (myGameboard) => {
+      const guess = getComputerAttack(myGameboard)
+      myGameboard.receiveAttack(guess)
+    },
+  }
+}
+
+function getComputerAttack() {
+  const guess = Math.floor(Math.random() * 100)
+  if (
+    myGameboard.coordinates[guess].value === 'missed' ||
+    myGameboard.coordinates[guess].value === 'hit'
+  ) {
+    getComputerAttack()
+  } else return myGameboard.coordinates[guess].name
+}
+
+const myGameboard = Gameboard()
+// const computer = Computer()
 // myGameboard.placeShip('submarine', 'a4')
 // myGameboard.placeShip('destroyer', 'c4')
 // myGameboard.placeShip('battleship', 'c10')
 // myGameboard.placeShip('carrier', 'i8')
 // myGameboard.receiveAttack('a5')
 // myGameboard.receiveAttack('a4')
+// myGameboard.receiveAttack('a2')
+// myGameboard.receiveAttack('a1')
+// myGameboard.receiveAttack('b2')
 // console.log(myGameboard.checkIfAllShipsAreSunk())
+// computer.attack(myGameboard)
 // console.log(myGameboard.coordinates)
-module.exports = { Ship, identifyShip, createGameboard: createGameboardCoords, Gameboard }
+
+module.exports = {
+  Ship,
+  identifyShip,
+  createGameboard: createGameboardCoords,
+  Gameboard,
+  Player,
+  Computer,
+}
