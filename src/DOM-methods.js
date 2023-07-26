d7147e131d001104675353c34d086ef17c14bab3
paplauskis <paplauskaskostas@gmail.com>
2023-07-26 14:35:36 +0300

    turned 3 coordinate creating functions into 1

--- src/DOM-methods.js
+++ src/DOM-methods.js
@@ -9,27 +9,26 @@ const {
 
 const playerGameboardDiv = document.querySelector('.player-gameboard')
 const computerGameboardDiv = document.querySelector('.computer-gameboard')
+const infoDiv = document.querySelector('.info-div')
 
-function appendPlayerCoordinateDivs(coord, number) {
-  const div = createCoordinateDivs(coord, number)
-  playerGameboardDiv.append(div)
+function createCoordinateDivs(coordinates, isPlayer) {
+  coordinates.forEach((coord, i = 0) => {
+    const div = document.createElement('div')
+    div.style.width = `${playerGameboardDiv.clientWidth / 10}px`
+    div.style.height = `${playerGameboardDiv.clientHeight / 10}px`
+    div.textContent = `${coord.name}`
+    div.name = coord.name
+    div.value = coord.value
+    div.classList.add(i++, isPlayer ? 'player-div' : 'computer-div', 'square-div')
+    if (!isPlayer) addClickEvents(div)
+    isPlayer ? playerGameboardDiv.append(div) : computerGameboardDiv.append(div)
+  })
 }
 
-function appendComputerCoordinateDivs(coord, number) {
-  const div = createCoordinateDivs(coord, number)
-  computerGameboardDiv.append(div)
+function addClickEvents(div) {
+  div.addEventListener('click', () => {
+    console.log(div.name)
+  })
 }
 
-function createCoordinateDivs(coord, number) {
-  const div = document.createElement('div')
-  div.style.width = `${playerGameboardDiv.clientWidth / 10}px`
-  div.style.height = `${playerGameboardDiv.clientHeight / 10}px`
-  div.textContent = `${coord}`
-  div.value = coord
-  div.classList.add('square-div')
-  div.classList.add(number)
-
-  return div
-}
-
-module.exports = { appendPlayerCoordinateDivs, appendComputerCoordinateDivs }
+module.exports = { createCoordinateDivs }
