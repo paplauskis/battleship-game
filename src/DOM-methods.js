3ef14ce2da7d693003a1d51ce3a9b348a815da34
paplauskis <paplauskaskostas@gmail.com>
2023-07-29 17:29:12 +0300

    moved functions from DOM-methods.js to game.js
    for testing purposes

--- src/DOM-methods.js
+++ src/DOM-methods.js
@@ -1,45 +1,13 @@
-const playerGameboardDiv = document.querySelector('.player-gameboard')
-const computerGameboardDiv = document.querySelector('.computer-gameboard')
-const infoDiv = document.querySelector('.info-div')
-const playerDivArray = []
-const computerDivArray = []
 
-function createCoordinateDivs(coordinates, isPlayer) {
-  coordinates.forEach((coord, i = 0) => {
-    const div = document.createElement('div')
-    div.style.width = `${playerGameboardDiv.clientWidth / 10}px`
-    div.style.height = `${playerGameboardDiv.clientHeight / 10}px`
-    div.textContent = `${coord.name}`
-    div.name = coord.name
-    div.value = coord.value
-    isPlayer ? playerDivArray.push(div.name) : computerDivArray.push(div.name)
-    div.classList.add(
-      i++,
-      isPlayer ? 'player-div' : 'computer-div',
-      'square-div'
-    )
-    if (!isPlayer) addClickEvents(div)
-    isPlayer ? playerGameboardDiv.append(div) : computerGameboardDiv.append(div)
-  })
-}
+// const playerDivArray = []
+// const computerDivArray = []
 
-function addClickEvents(div) {
-  div.addEventListener('click', () => {
-    console.log(getDivInfo(div))
-  })
-}
 
-function getDivInfo(div) {
-  div.value === null
-    ? (div.style.backgroundColor = 'green')
-    : (div.style.backgroundColor = 'red')
-  return div
-}
 
-module.exports = {
-  createCoordinateDivs,
-  addClickEvents,
-  playerDivArray,
-  computerDivArray,
-  computerGameboardDiv,
-}
+// module.exports = {
+//   createCoordinateDivs,
+//   addClickEvents,
+//   playerDivArray,
+//   computerDivArray,
+//   computerGameboardDiv,
+// }
--- src/game.js
+++ src/game.js
@@ -1,10 +1,29 @@
-const { Ship, Gameboard } = require('./gameboard')
-const { createCoordinateDivs } = require('./DOM-methods')
+const { Gameboard } = require('./gameboard')
+// const { createCoordinateDivs, getDivInfo } = require('./DOM-methods')
 const { Player, Computer } = require('./player-computer')
 const { selectLetterCoord, selectRandomNumber } = require('./helper-functions')
+const playerGameboardDiv = document.querySelector('.player-gameboard')
+const computerGameboardDiv = document.querySelector('.computer-gameboard')
+const infoDiv = document.querySelector('.info-div')
+const endDisplay = document.querySelector('.blur-div')
+const winnerDiv = document.querySelector('.winner')
+const newGameBtn = document.querySelector('.new-game')
 
-function togglePlayers(activePlayer, player, computer) {
-  activePlayer === player ? computer : player
+let gameOver = false
+let playerTurn = true
+endDisplay.style.display = 'none'
+
+const player = Player()
+const playerGameboard = Gameboard()
+const computer = Computer()
+const computerGameboard = Gameboard()
+placeShipsOnCoords(computerGameboard)
+placeShipsOnCoords(playerGameboard)  
+createCoordinateDivs(playerGameboard.coordinates, true)
+createCoordinateDivs(computerGameboard.coordinates, false)
+
+function toggleTurn() {
+  playerTurn ? playerTurn = false : playerTurn = true
 }
 
 function placeShipsOnCoords(gameboard) {
@@ -16,16 +35,52 @@ function placeShipsOnCoords(gameboard) {
   gameboard.placeShip('destroyer', letters[3] + numbers[3])
 }
 
+function createCoordinateDivs(coordinates, isPlayer) {
+  coordinates.forEach((coord, i = 0) => {
+    const div = document.createElement('div')
+    div.style.width = `${playerGameboardDiv.clientWidth / 10}px`
+    div.style.height = `${playerGameboardDiv.clientHeight / 10}px`
+    div.textContent = `${coord.name}`
+    div.name = coord.name
+    div.value = coord.value
+    // isPlayer ? playerDivArray.push(div.name) : computerDivArray.push(div.name)
+    div.classList.add(
+      i++,
+      isPlayer ? 'player-div' : 'computer-div',
+      'square-div'
+    )
+    if (!isPlayer) addClickEvents(div)
+    isPlayer ? playerGameboardDiv.append(div) : computerGameboardDiv.append(div)
+  })
+}
+
+function addClickEvents(div) {
+  div.addEventListener('click', () => {
+    handleClick(div)
+  })
+}
+
+function handleClick(div) {
+  div.value === null
+    ? (div.style.backgroundColor = 'green')
+    : (div.style.backgroundColor = 'red')
+  console.log(div.name)
+  player.attack(computerGameboard, div.name)
+  console.log(computerGameboard.checkIfAllShipsAreSunk())
+}
+
+function showResults() {
+  const winner = computerGameboard.checkIfAllShipsAreSunk() ? 'Player wins!' : 'Computer wins!'
+  const endDiv = document.createElement('div') 
+  endDiv.classList.add('end-div')
+  
+
+
+}
+
 function Game() {
-  const player = Player()
-  const playerGameboard = Gameboard()
-  const computer = Computer()
-  const computerGameboard = Gameboard()
-  createCoordinateDivs(playerGameboard.coordinates, true)
-  createCoordinateDivs(computerGameboard.coordinates, false)
-  let activePlayer = player
-  placeShipsOnCoords(computerGameboard)
-  placeShipsOnCoords(playerGameboard)
+ 
+
 }
 
 module.exports = { Game, selectRandomNumber }
